75b40dcb2170bd35b6f6176c87ada47e5cb4e3b4
Chandan Kumar Sahoo <chandankumarsahoo171@gmail.com>
2024-10-11 08:34:19 +0530

    first commit

---
+++ src/InfoBox.jsx
@@ -0,0 +1,43 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css";
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
+export default function InfoBox({ info }) {
+    const INIT_URL = "https://images.unsplash.com/photo-1672226405717-697c84f48f9e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGR1c3R5JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+
+    const HOT_URL = "https://images.unsplash.com/photo-1649960861742-11fcecbc75dc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHN1bm55JTIwaG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+    const COLD_URL = "https://plus.unsplash.com/premium_photo-1668792545129-72d876248c1b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29sZCUyMGRheSUyMGltYWdlfGVufDB8fDB8fHww";
+    const RAIN_URL = "https://plus.unsplash.com/premium_photo-1664299041161-25e40a5feac0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTY1fHxyYWlueSUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+
+    return (
+        <div className="InfoBox">
+            <div className="cardContainer">
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image={info.humidity > 80 ? RAIN_URL : info.temp > 15 ? HOT_URL : COLD_URL}
+                        title="green iguana"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {info.city} {info.humidity > 80 ? <ThunderstormIcon /> : info.temp > 15 ? <WbSunnyIcon /> : <AcUnitIcon />}
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
+                            <p>Temperature = {info.temp}&deg;C</p>
+                            <p>Humidity = {info.humidity}</p>
+                            <p>Min Temp = {info.tempMin}&deg;C</p>
+                            <p>Max Temp = {info.tempMax}&deg;C</p>
+                            <p>The Weather can be described as <i> {info.weather} </i> and Feels like {info.feelsLike}&deg;C</p>
+                        </Typography>
+                    </CardContent>
+
+                </Card>
+            </div>
+        </div>
+    );
+}
